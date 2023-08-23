0376f9694e5c7f9a7cb7a0c5ba628c7f6ee9f8a1
Nabeel-Mufin <124709075+Nabeel-Mufin@users.noreply.github.com>
2023-08-23 04:55:17 +0000

    fix the link

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,7 +12,7 @@ export default function DashboardLayout({
         <div className="!min-h-screen w-full bg-[#CEDFF9]">
             <div className="gap-4 pr-6 pl-7 py-3 !bg-[#eff2f8]">
                 <p>I am layout</p>
-                <Link href='child-route'>Some child route inside layout</Link>
+                <Link href='/dashboard/child-route'>Some child route inside layout</Link>
             </div>
         </div>
     );
